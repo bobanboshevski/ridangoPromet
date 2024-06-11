f3f5681506a992b068d322801e87bc833e5bdb4b
bobanboshevski <152434305+bobanboshevski@users.noreply.github.com>
2024-06-11 14:06:26 +0200

    commented main parts

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
         //AvtobusniPromet avtobuskaPostaja = new AvtobusniPromet();
         AvtobusniPromet avtobusniPromet = AvtobusniPromet.getInstance();
 
-        System.out.println(" - - - AVTOBUSNE POSTAJE - - - ");
-        //avtobusniPromet.prikazStops();
-
-//        System.out.println(" - - - AVTOBUSE NA VSE POSTAJE - - - ");
-//        avtobuskaPostaja.prikazStopTimes();
-
-        System.out.println(" - - - - - - - - - - ");
+//        System.out.println(" - - - AVTOBUSNE POSTAJE - - - ");
+//        //avtobusniPromet.prikazStops();
+//
+////        System.out.println(" - - - AVTOBUSE NA VSE POSTAJE - - - ");
+////        avtobuskaPostaja.prikazStopTimes();
+//
+//        System.out.println(" - - - - - - - - - - ");
 
 
         //avtobuskaPostaja.getAvtobusneLinijeZaPostajo(10, 5, "absolute" );
